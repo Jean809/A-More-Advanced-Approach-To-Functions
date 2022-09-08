3d5baf4d6ff092e37e7060f0e3b223b6b6741648
Jean809 <jean_carlosrosario709@outlook.com>
2022-09-07 21:59:05 -0400

    Functions accepting callback functions

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 // --------------------------------How Passing Arguments Works: Values vs. Reference --------------------------------------------------
 
+/*
+
 const flight = "LH234";
 const jonas = {
   name: "Jonas Schmedtmann",
@@ -33,3 +35,46 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+*/
+
+// ---------------------------- Functions accepting CallBack fucntions ------------------------------------------------------
+
+//Example 1
+
+function firstWordUpperCase(str) {
+  const [first, ...rest] = str.split(" ");
+  return `${first.toUpperCase()} ${rest.join(" ")}`;
+}
+
+function transformer(str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+  console.log(`Transformed by: ${fn.name}`);
+}
+
+transformer("Javascript is fun! Sometimes...", firstWordUpperCase);
+
+console.log("-------------------------EXAMPLE 2-------------------------");
+
+//Example 2
+const grades = [68, 71, 92, 50, 72];
+grades.name = "Jean";
+
+function gradeAvg(arr) {
+  let sum = 0;
+  for (const gradesAVG of arr) {
+    sum += gradesAVG;
+  }
+  return sum / arr.length;
+}
+
+function passOrFail(arr, fn) {
+  if (fn(arr) >= 80) {
+    console.log(`Student ${arr.name} has passed the course!`);
+  } else {
+    console.log(`Student ${arr.name} has failed the course.`);
+  }
+}
+
+passOrFail(grades, gradeAvg);
